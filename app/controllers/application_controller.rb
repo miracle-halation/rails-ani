c9203c7299809f7e95fda171b7bab9e4d89e04f7
miracle-halation <miraclehalation.9710@gmail.com>
2021-09-01 16:56:22 +0900

    iconのパラメーターを通す

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,6 @@ class ApplicationController < ActionController::API
 	protected
 
 	def configure_permitted_parameters
-		devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
+		devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :icon])
 	end
 end
