2d81cde5e20fe23a55c9da30b42ba5bf16bb3c4d
miracle-halation <miraclehalation.9710@gmail.com>
2021-09-23 11:32:25 +0900

    名前ミスあったので削除

--- app/models/room_user.rb
+++ app/models/room_user.rb
@@ -1,4 +1,2 @@
 class RoomUser < ApplicationRecord
-	belongs_to :user
-	belongs_to :room
 end
--- db/migrate/20210923021322_create_room_users.rb
+++
@@ -1,9 +0,0 @@
-class CreateRoomUsers < ActiveRecord::Migration[6.1]
-  def change
-    create_table :room_users do |t|
-      t.references  :user,  index: true, foreign_key: true
-      t.references  :room,  index: true, foreign_key: true
-      t.timestamps
-    end
-  end
-end
