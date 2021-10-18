f383f4005d3557c271bfcfdcf60dd42d7f672f77
miracle-halation <miraclehalation.9710@gmail.com>
2021-10-18 21:32:31 +0900

    tagsテーブルのカラム名ミスを修正

--- db/schema.rb
+++ db/schema.rb
@@ -59,7 +59,7 @@ ActiveRecord::Schema.define(version: 2021_10_15_035820) do
   end
 
   create_table "tags", charset: "utf8mb4", force: :cascade do |t|
-    t.string "CreateTags", null: false
+    t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
