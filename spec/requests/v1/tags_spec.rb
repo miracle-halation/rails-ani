c5ea4c70c42c8f5693f1c08711c4f1aaba5f1f2a
miracle-halation <miraclehalation.9710@gmail.com>
2021-10-30 10:51:04 +0900

    tagコントローラーのテストを作成

--- spec/requests/v1/tags_spec.rb
+++ spec/requests/v1/tags_spec.rb
@@ -1,7 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe "V1::Tags", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe "GET /show" do
+    let(:tag) { FactoryBot.create(:tag, name: "テスト確認タグ") }
+    let(:user) { FactoryBot.create(:user, tags: [tag]) }
+    it "ユーザーに紐づいているタグを取得して返す" do
+      get v1_tag_path(user.id)
+      json = JSON.parse(response.body)
+      expect(response.status).to eq(200)
+      expect(json['data'][0]['name']).to eq('テスト確認タグ')
+    end
   end
 end
