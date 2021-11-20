33dbe8354330d740d6768873af80b0e3a22993a5
miracle-halation <miraclehalation.9710@gmail.com>
2021-11-21 00:13:29 +0900

    Action Cableで双方向通信を実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  mount ActionCable.server => '/cable'
   mount_devise_token_auth_for 'User', at: 'auth', controllers: {
     registrations: 'auth/registrations',
     sessions: 'auth/sessions'
