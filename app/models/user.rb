297de92058bb45908478c7a398122e048ad90193
miracle-halation <miraclehalation.9710@gmail.com>
2021-11-25 22:41:15 +0900

    フレンド系のメソッドを一式実装
    動作確認済み

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,9 +12,9 @@ class User < ActiveRecord::Base
   has_many :user_tags, dependent: :destroy
   has_many :tags, through: :user_tags
   has_many :messages
-  has_many :friend_relationships, foreign_key: 'friend_id', class_name: "User", dependent: :destroy
+  has_many :friend_relationships, foreign_key: 'friend_id', class_name: "Friend", dependent: :destroy
   has_many :friends, through: :friend_relationships
-  has_many :applicant_relationships, foreign_key: 'applicant', class_name: "User", dependent: :destroy
+  has_many :applicant_relationships, foreign_key: 'applicant_id', class_name: "Friend", dependent: :destroy
   has_many :applicants, through: :applicant_relationships
   has_one_attached :icon
   validates :nickname, presence: true, length: { maximum: 10 }
@@ -34,4 +34,31 @@ class User < ActiveRecord::Base
   def icon_url
     icon.attached? ? url_for(icon) : nil
   end
+
+  def friend?(other_id)
+    applicant_relationships.find_by(friend_id: other_id)
+  end
+
+  def accept?(other_id)
+    friend = applicant_relationships.find_by(friend_id: other_id)
+    return friend.accept
+  end
+
+  def approval_pending(current_user, other_id)
+    applicant_relationships.create(applicant_id: current_user,friend_id: other_id)
+    friend_relationships.create(applicant_id: other_id,friend_id: current_user)
+  end
+
+  def accept!(current_user, other_id)
+    own = applicant_relationships.where("(applicant_id = ?) AND (friend_id = ?) AND (accept = ?)", current_user, other_id, false)
+    own.update(accept: true)
+    friend = friend_relationships.where("(applicant_id = ?) AND (friend_id = ?) AND (accept = ?)", other_id, current_user, false)
+    friend.update(accept: true)
+  end
+
+  def del_friend(current_user, other_id)
+    applicant_relationships.where("(applicant_id = ?) AND (friend_id = ?)", current_user, other_id).first.destroy
+    friend_relationships.where("(applicant_id = ?) AND (friend_id = ?)", other_id, current_user).first.destroy
+  end
+
 end
