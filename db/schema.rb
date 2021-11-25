1703c446ec5c8c518d88b7fcf1aa1a7849f365ad
miracle-halation <miraclehalation.9710@gmail.com>
2021-11-25 21:54:32 +0900

    名前を変更

--- db/schema.rb
+++ db/schema.rb
@@ -41,13 +41,13 @@ ActiveRecord::Schema.define(version: 2021_11_24_131358) do
   end
 
   create_table "friends", charset: "utf8mb4", force: :cascade do |t|
-    t.integer "applicant"
+    t.integer "applicant_id"
     t.integer "friend_id"
     t.boolean "accept", default: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["applicant", "friend_id"], name: "index_friends_on_applicant_and_friend_id", unique: true
-    t.index ["applicant"], name: "index_friends_on_applicant"
+    t.index ["applicant_id", "friend_id"], name: "index_friends_on_applicant_id_and_friend_id", unique: true
+    t.index ["applicant_id"], name: "index_friends_on_applicant_id"
     t.index ["friend_id"], name: "index_friends_on_friend_id"
   end
 
