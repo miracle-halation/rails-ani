b5dc1d910c5ed0268fff1373f1994dbba9c2673f
miracle-halation <miraclehalation.9710@gmail.com>
2022-09-07 21:21:52 +0900

    ログイン周りのテストを修正

--- spec/requests/auth/registrations_spec.rb
+++ spec/requests/auth/registrations_spec.rb
@@ -28,45 +28,14 @@ RSpec.describe 'Auth::Registrations', type: :request do
     end
   end
 
-  describe 'Post /auth/sign_in' do
-    let!(:user) { FactoryBot.create(:user) }
-    context '値が正しいとき' do
-      it '認証に成功し、成功したデータを返す' do
-        test_params = { email: user.email, password: 'testtest' }
-        post '/auth/sign_in', params: test_params
-        json = JSON.parse(response.body)
-        expect(response.status).to eq(200)
-        expect(json['data']['email']).to eq(user.email)
-        expect(json['data']['nickname']).to eq(user.nickname)
-      end
-    end
-    context '値が間違っているとき' do
-      it 'emailが違うと認証に失敗し、Invalidを返す' do
-        test_params = { email: 'test_failed@gmail.com', password: 'testtest' }
-        post '/auth/sign_in', params: test_params
-        json = JSON.parse(response.body)
-        expect(response.status).to eq(401)
-        expect(json['success']).to be_falsey
-        expect(json['errors'][0]).to eq('Invalid login credentials. Please try again.')
-      end
-      it 'passwordが違うと認証に失敗し、Invalidを返す' do
-        test_params = { email: 'test@gmail.com', password: 'password_failed' }
-        post '/auth/sign_in', params: test_params
-        json = JSON.parse(response.body)
-        expect(response.status).to eq(401)
-        expect(json['success']).to be_falsey
-        expect(json['errors'][0]).to eq('Invalid login credentials. Please try again.')
-      end
-    end
-  end
-
   describe 'Put /auth' do
     let!(:user) { FactoryBot.create(:user) }
+    let!(:auth_headers) { login(user) }
     context '値が正しいとき' do
       it 'userの情報を更新することができる' do
         test_params = { id: user.id, address: 'アップデートテスト団地', myinfo: 'アップデートテスト自己紹介', tags: 'テストタグ, テスト２タグ' }
         expect(user.address).not_to eq('アップデートテスト団地')
-        put '/auth', params: test_params
+        put '/auth', params: test_params, headers: auth_headers
         json = JSON.parse(response.body)
         expect(response.status).to eq(200)
         expect(json['data']['address']).to eq('アップデートテスト団地')
@@ -77,9 +46,10 @@ RSpec.describe 'Auth::Registrations', type: :request do
 
   describe 'Delete /auth' do
     let!(:user) { FactoryBot.create(:user) }
+    let!(:auth_headers) { login(user) }
     it 'userを削除することができる' do
       test_params = { id: user.id }
-      expect { delete '/auth', params: test_params }.to change(User, :count).by(-1)
+      expect { delete '/auth', params: test_params, headers: auth_headers }.to change(User, :count).by(-1)
     end
   end
 end
--- spec/requests/auth/sessions_spec.rb
+++ spec/requests/auth/sessions_spec.rb
@@ -1,6 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe 'Auth::Sessions', type: :request do
-  describe 'GET /index' do
+  describe 'Post /auth/sign_in' do
+    let!(:user) { FactoryBot.create(:user) }
+    context '値が正しいとき' do
+      it '認証に成功し、成功したデータを返す' do
+        test_params = { email: user.email, password: 'testtest' }
+        post '/auth/sign_in', params: test_params
+        json = JSON.parse(response.body)
+        expect(response.status).to eq(200)
+        expect(json['data']['email']).to eq(user.email)
+        expect(json['data']['nickname']).to eq(user.nickname)
+      end
+    end
+    context '値が間違っているとき' do
+      it 'emailが違うと認証に失敗し、Invalidを返す' do
+        test_params = { email: 'test_failed@gmail.com', password: 'testtest' }
+        post '/auth/sign_in', params: test_params
+        json = JSON.parse(response.body)
+        expect(response.status).to eq(401)
+        expect(json['success']).to be_falsey
+        expect(json['errors'][0]).to eq('Invalid login credentials. Please try again.')
+      end
+      it 'passwordが違うと認証に失敗し、Invalidを返す' do
+        test_params = { email: 'test@gmail.com', password: 'password_failed' }
+        post '/auth/sign_in', params: test_params
+        json = JSON.parse(response.body)
+        expect(response.status).to eq(401)
+        expect(json['success']).to be_falsey
+        expect(json['errors'][0]).to eq('Invalid login credentials. Please try again.')
+      end
+    end
   end
 end
