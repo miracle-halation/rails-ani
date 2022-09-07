6a4a5675b3720edcb558b05219d8ae01c09d5617
miracle-halation <miraclehalation.9710@gmail.com>
2022-09-07 22:02:44 +0900

    friendコントローラーの修正。テストもパスする

--- app/controllers/v1/friends_controller.rb
+++ app/controllers/v1/friends_controller.rb
@@ -1,22 +1,20 @@
 class V1::FriendsController < ApplicationController
   before_action :authenticate_user!
-  before_action :find_user, only: [:show, :destroy]
-  before_action :current_user, only: [:apply, :approval]
 
   def show
-    friends = @user.applicants.joins(:friends).select('users.*, friends.accept')
-    pending_friends = @user.friends.joins(:applicants).select('users.*, friends.accept').uniq
+    friends = current_user.applicants.joins(:friends).select('users.*, friends.accept')
+    pending_friends = current_user.friends.joins(:applicants).select('users.*, friends.accept').uniq
     render json: { status: 'SUCCESS', data: [friends, pending_friends] }
   end
 
   def destroy
-    @user.del_friend(@user.id, params[:friend_id])
+    current_user.del_friend(current_user.id, params[:friend_id])
     render json: { status: 'SUCCESS', data: 'フレンド解除に成功しました', color: 'green' }
   end
 
   def apply
-    if @user.friend?(params[:friend_id])
-      @user.approval_pending(@user.id, params[:friend_id])
+    if current_user.friend?(params[:friend_id])
+      current_user.approval_pending(current_user.id, params[:friend_id])
       render json: { status: 'SUCCESS', data: 'フレンド申請に成功しました', color: 'green' }
     else
       render json: { status: 'ERROR', data: 'すでにフレンド登録されています', color: 'red' }
@@ -25,21 +23,11 @@ class V1::FriendsController < ApplicationController
 
   def approval
     friend = User.find(params[:friend_id])
-    if @user.accept?(params[:friend_id])
+    if current_user.accept?(params[:friend_id])
       render json: { status: 'ERROR', data: '承認に失敗しました', color: 'red' }
     else
-      @user.accept!(@user.id, friend)
+      current_user.accept!(current_user.id, friend)
       render json: { status: 'SUCCESS', data: '承認に成功しました', color: 'green' }
     end
-  end
-
-  private
-
-  def find_user
-    @user = User.find(params[:id])
-  end
-
-  def current_user
-    @user = User.find(params[:user_id])
   end
 end
--- spec/requests/v1/friends_spec.rb
+++ spec/requests/v1/friends_spec.rb
@@ -1,23 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe 'V1::Friends', type: :request do
+  # ログインユーザー
   let!(:user) { FactoryBot.create(:user) }
-  let!(:friend_user) { FactoryBot.create(:user) }
+  let!(:auth_headers) { login(user) }
+
+  # フレンド承認済みユーザー
+  let!(:apply_user) { FactoryBot.create(:user) }
+  let!(:apply_friend) { FactoryBot.create(:friend, friend_id: user.id, applicant_id: apply_user.id, accept: 1) }
+  let!(:applicant_friend) { FactoryBot.create(:friend, friend_id: apply_user.id, applicant_id: user.id, accept: 1) }
+
+  # フレンド保留中
   let!(:pending_friend) { FactoryBot.create(:friend, friend_id: user.id) }
-  let!(:friend) { FactoryBot.create(:friend, friend_id: user.id, applicant_id: friend_user.id, accept: 1) }
-  let!(:applicant_friend) { FactoryBot.create(:friend, friend_id: friend_user.id, applicant_id: user.id, accept: 1) }
+
   describe 'GET /show' do
     it '正しい値を返している' do
-      get v1_friend_path(user.id)
+      get v1_friend_path(user.id), headers: auth_headers
       json = JSON.parse(response.body)
+      # 承認済みフレンド一覧
+      apply_friends = json['data'][0]
+      apply_friends_ids = apply_friends.map{ |value| value['id'] }
+      # 保留中フレンド一覧
+      pending_friends = json['data'][1]
+      pending_friends_ids = pending_friends.map{ |value| value['id'] }
+
       expect(response.status).to eq(200)
-      expect(json['data'][0][0]['id']).to eq(friend_user.id)
-      expect(json['data'][1][1]['id']).to eq(pending_friend.applicant_id)
+      expect(apply_friends_ids).to include(apply_user.id)
+      expect(pending_friends_ids).to include(pending_friend.applicant_id)
     end
   end
   describe 'DELETE /destroy' do
     it 'フレンドを削除することができる' do
-      expect { delete v1_friend_path(user.id, friend_id: friend_user.id) }.to change(Friend, :count).by(-2)
+      expect { delete v1_friend_path(user.id, friend_id: apply_user.id), headers: auth_headers }.to change(Friend, :count).by(-2)
       json = JSON.parse(response.body)
       expect(json['status']).to eq('SUCCESS')
       expect(json['data']).to eq('フレンド解除に成功しました')
@@ -28,7 +42,7 @@ RSpec.describe 'V1::Friends', type: :request do
     let!(:pending_user) { FactoryBot.create(:user) }
     context '成功するとき' do
       it 'friendテーブルが増えて、指定の値を返す' do
-        expect { post apply_v1_friends_path(user_id: user.id, friend_id: pending_user.id) }.to change(Friend, :count).by(1)
+        expect { post apply_v1_friends_path(user_id: user.id, friend_id: pending_user.id), headers: auth_headers }.to change(Friend, :count).by(1)
         json = JSON.parse(response.body)
         expect(json['status']).to eq('SUCCESS')
         expect(json['data']).to eq('フレンド申請に成功しました')
@@ -37,7 +51,7 @@ RSpec.describe 'V1::Friends', type: :request do
     end
     context '失敗するとき' do
       it 'friendテーブルが増えず、指定の値を返す' do
-        expect { post apply_v1_friends_path(user_id: user.id, friend_id: friend_user.id) }.to change(Friend, :count).by(0)
+        expect { post apply_v1_friends_path(user_id: user.id, friend_id: apply_user.id), headers: auth_headers }.to change(Friend, :count).by(0)
         json = JSON.parse(response.body)
         expect(json['status']).to eq('ERROR')
         expect(json['data']).to eq('すでにフレンド登録されています')
@@ -50,7 +64,7 @@ RSpec.describe 'V1::Friends', type: :request do
     let!(:approval_friend) { FactoryBot.create(:friend, friend_id: user.id, applicant_id: pending_user.id) }
     context '成功するとき' do
       it 'friendテーブルが増えず、指定の値を返す' do
-        expect { post approval_v1_friends_path(user_id: user.id, friend_id: pending_user.id) }.to change(Friend, :count).by(1)
+        expect { post approval_v1_friends_path(user_id: user.id, friend_id: pending_user.id), headers: auth_headers }.to change(Friend, :count).by(1)
         expect(applicant_friend.accept).to be true
         json = JSON.parse(response.body)
         expect(json['status']).to eq('SUCCESS')
@@ -60,7 +74,7 @@ RSpec.describe 'V1::Friends', type: :request do
     end
     context '失敗するとき' do
       it 'friendテーブルが増えず、指定の値を返す' do
-        expect { post approval_v1_friends_path(user_id: user.id, friend_id: friend_user.id) }.to change(Friend, :count).by(0)
+        expect { post approval_v1_friends_path(user_id: user.id, friend_id: apply_user.id), headers: auth_headers }.to change(Friend, :count).by(0)
         json = JSON.parse(response.body)
         expect(json['status']).to eq('ERROR')
         expect(json['data']).to eq('承認に失敗しました')
