97bd0ab59fae6b3ec1769eeb0ef8b58026dbbf2c
miracle-halation <miraclehalation.9710@gmail.com>
2022-09-07 22:18:01 +0900

    tagsコントローラーを修正

--- spec/requests/v1/tags_spec.rb
+++ spec/requests/v1/tags_spec.rb
@@ -4,8 +4,9 @@ RSpec.describe 'V1::Tags', type: :request do
   describe 'GET /show' do
     let(:tag) { FactoryBot.create(:tag, name: 'テスト確認タグ') }
     let(:user) { FactoryBot.create(:user, tags: [tag]) }
+    let!(:auth_headers) { login(user) }
     it 'ユーザーに紐づいているタグを取得して返す' do
-      get v1_tag_path(user.id)
+      get v1_tag_path(user.id), headers: auth_headers
       json = JSON.parse(response.body)
       expect(response.status).to eq(200)
       expect(json['data'][0]['name']).to eq('テスト確認タグ')
