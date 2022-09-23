23ce8f9e45abcfaeba25e06163b89aef35151500
miracle-halation <miraclehalation.9710@gmail.com>
2022-09-23 10:11:12 +0900

    返却値をログインしているユーザーのroomのみに変更

--- app/controllers/v1/rooms_controller.rb
+++ app/controllers/v1/rooms_controller.rb
@@ -3,9 +3,8 @@ class V1::RoomsController < ApplicationController
   before_action :find_room, except: [:index, :new, :create, :search]
 
   def index
-    @rooms = Room.all
-    @favorite_rooms = Room.joins(:room_users).group(:room_id).order('count(user_id) desc')
-    render json: { status: 'Success', data: [@rooms, @favorite_rooms] }
+    @rooms = current_user.rooms
+    render json: { status: 'Success', data: [@rooms] }
   end
 
   def new
